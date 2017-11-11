c707f02fd693d4f36edad1515029c713429eb49e
Anton Osika <anton.osika@gmail.com>
2017-11-11 16:43:08 +0100

    Working on betago interfacing. Action space should be changed to be constant! Also, network functions should be replaced contained in an object.

--- agz.py
+++ agz.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+import betago
+from betago.dataloader.goboard import GoBoard
+
+BOARD_SIZE = 9
 ACTION_SPACE = [9*9 + 1]
 C_PUCT = 1.0
 
@@ -13,17 +17,76 @@ TODO/fix:
 - Render play with opponent
 - Render self play from history
 - Implement NN, and training NN
+- make networks into a class
+- Compute winner from board
 
 """
 
+
+class GoState(GoBoard):
+
+    def __init__(self, board_size=BOARD_SIZE):
+        super(GoBoard, self).__init__(board_size)
+
+        self.game_over = False
+        self.winner = None
+        self.action_space = list(range(board_size**2 + 1))
+        self.current_player = 1  # in (1, -1)
+
+        self.last_action = None
+        self.last_action_2 = None
+
+    def step(self, action):
+        random_ordering = iter(np.random.permutation(len(self.action_space)))
+
+        # Find first legal move:
+        while not self.is_move_legal(self.current_player, action):
+            try:
+                action_index = next(random_ordering)
+            except:
+                raise Exception("No legal action.")
+            action = self.action_space[action_index]
+
+        super(GoBoard, self).apply_move(action, self.current_player)
+
+        self.current_player = -self.current_player
+        self.check_state()
+        self.game_over = self._game_over()
+        self.winner =
+
+        self.last_action_2 = self.last_action
+        self.last_action = action
+
+    def _new_state_checks(self):
+        full_board = len(self.board) == self.board_size**2
+        double_pass = (self.last_action == len(self.action_space) - 1) and \
+                      (self.last_last_2 == self.last_action)
+        self.game_over = full_board or double_pass
+        if self.game_over:
+            self.winner = self._compute_winner()
+
+    def _winner(self):
+        return 1
+
+
 def step(state, action):
-    """Game env placeholder"""
+    """
+
+    Args:
+        state: GoState
+    """
+
+    new_state = deepcopy.copy(state)
+    new_state.step()
     return state
 
 def policy_network(state):
     """Returns distribution over all allowed actions"""
     # uniform placeholder:
     return np.zeros_like(state.action_space) + 1.0/len(state.action_space)
+
+    all_action_probs = model.predict(state)
+    return all_action_probs[state.action_space]  # -> gets the probabilities that are allowed by indices
 
 def value_network(state):
     """Returns value of position for player 1."""
@@ -48,7 +111,7 @@ class TreeStructure():
 
         self.q = np.zeros_like(state.action_space)
         self.n = np.zeros_like(state.action_space) + np.finfo(np.float).resolution
-        self.policy_result = policy_network(state)
+        self.policy_result = policy_network(state)  # TODO: use a setter function
 
         self.n_passed = 0
         state.action_that_led_here = action_that_led_here
@@ -113,6 +176,13 @@ def play_game(state=START_STATE, opponent=None):
             while action in node.children.keys():
                 node = node.children[action]
                 action = puct_action(node)
+
+            if node.state.game_over:
+                # This only happens the second time we go to a winning state.
+                # Logic for visiting "winning nodes" multiple times is probably correct? FIXME
+                value = node.state.winner
+                backpropagate(node, value)
+                continue
             
             # Expand tree:
             new_state = play_action(node.state, action)
@@ -130,7 +200,7 @@ def play_game(state=START_STATE, opponent=None):
             backpropagate(node, value)
 
         # Store the state and distribution before we prune the tree:
-        game_history.append([tree_root.state, tree_root.n])
+        game_history.append([tree_root.state, tree_root.n/tree_root.n.sum()])
 
         action = action_to_play(tree_root, opponent)
         tree_root = tree_root.children[action]
